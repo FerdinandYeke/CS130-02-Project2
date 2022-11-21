5da3aea1fd085059c52cdb1710aa592a0dd1f84d
FerdinandYeke <112411799+FerdinandYeke@users.noreply.github.com>
2022-11-21 12:40:34 -0500

    Add files via upload

---
+++ Delta_Airlines_SDF.java
@@ -0,0 +1,568 @@
+package Project2;
+
+import java.util.Scanner;
+import java.text.*;
+
+public class Delta_Airlines_SDF extends SDF_Route {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		NumberFormat fmt1 = NumberFormat.getCurrencyInstance();
+		Scanner scan = new Scanner(System.in);
+		SDF_Route s1 = new SDF_Route();
+		double ticket_price = 50.00;
+		
+		s1.display_route();
+		System.out.println("Please enter the Following Choices to progress: "
+				+ "\n1.ATL \n2.LGA \n3.DTW \n4.MSP");
+		
+		int choice = scan.nextInt();
+		
+		switch(choice)
+		{
+		case 1:
+			{
+			
+				System.out.println("You've entered: ATL");
+				System.out.println("Enter the amount of days to travel "
+						+ "\nfor a Roundtrip: ");
+				
+				int days = scan.nextInt();
+				
+				if(days != 0 && days <= 5 && days <= 10)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price));
+					
+						System.out.println("Do you want to book this flight?");
+						System.out.println("Y for Yes; N for No: ");
+						char Ans = scan.next().charAt(0);
+						
+						if(Ans == 'Y')
+						{
+							s1.Date();
+							s1.FlightNum1();
+							System.out.println("The ticket price: "+fmt1.format(ticket_price));
+							System.out.println("Your Date of Booking: "+s1.getDate());
+							System.out.println("Days for stay: "+days);
+							System.out.println("Airport Destination: ATL");
+							
+						}
+						else if(Ans == 'N') {
+							System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+						}
+				}
+				
+				else if(days <= 10 && days <= 15)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+15));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						
+						System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: ATL");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+					
+				}
+				
+				else if(days <= 15 && days <= 20)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+16));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: ATL");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+					
+				}
+				
+				else if(days <= 20 && days <= 30)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+119));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: ATL");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 62)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+190));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: ATL");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				
+				else
+				{
+					System.out.println("Exceeded the amount of days for a "
+							+ " road trip; which is 62. Please enter again");
+				}	
+					
+			}
+				break;
+
+		case 2:
+			{
+				System.out.println("You've entered: LGA");
+				System.out.println("Enter the amount of days to travel "
+						+ "\nfor a Roundtrip: ");
+				
+				int days = scan.nextInt();
+				
+				if(days != 0 && days <= 5 && days <= 10)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: LGA");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 10 && days <= 15)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+105));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.Date();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: LGA");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 15 && days <= 20)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+216));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.Date();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: LGA");
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 20 && days <= 30)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+219));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.Date();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: LGA");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if( days <= 62)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+220));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.Date();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: LGA");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else
+				{
+					System.out.println("Exceeded the amount of days for a "
+							+ " road trip; which is 62. Please enter again");
+				}
+				break;
+			}
+		
+		case 3:
+			{
+				System.out.println("You've entered: DTW");
+				System.out.println("Enter the amount of days to travel "
+						+ "\nfor a Roundtrip: ");
+				
+				int days = scan.nextInt();
+				
+				if(days != 0 && days <= 5 && days <= 10)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.Date();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: DTW");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 10 && days <= 15)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+105));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: DTW");
+						
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 15 && days <= 20)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+216));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: DTW");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 20 && days <= 30)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+219));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: DTW");
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if( days <= 62)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ " days are: "+fmt1.format(ticket_price+220));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: DTW");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else
+				{
+					System.out.println("Exceeded the amount of days for a "
+							+ "road trip; which is 62. Please enter again");
+				}
+				break;
+			}
+			
+		case 4:
+			{
+				System.out.println("You've entered: MSP");
+				System.out.println("Enter the amount of days to travel "
+						+ "\nfor a Roundtrip: ");
+				
+				int days = scan.nextInt();
+				
+				if(days != 0 && days <= 5 && days <= 10)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ "days are:"+fmt1.format(ticket_price));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: MSP");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 10 && days <= 15)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ "days are: "+fmt1.format(ticket_price+105.02));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: MSP");
+						
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 15 && days <= 20)
+				{
+					System.out.println("The Prices of the ticket on "+days+""
+							+ "days are:"+fmt1.format(ticket_price+216.02));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: MSP");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 20 && days <= 30)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ "days are: "+fmt1.format(ticket_price+219.02));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: MSP");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else if(days <= 62)
+				{
+					System.out.println("The Prices of the ticket on "+days+" "
+							+ "days are: "+fmt1.format(ticket_price+220.02));
+					System.out.println("Do you want to book this flight?");
+					System.out.println("Y for Yes; N for No: ");
+					char Ans = scan.next().charAt(0);
+					
+					if(Ans == 'Y')
+					{
+						s1.Date();
+						s1.FlightNum1();
+						s1.FlightNum1();System.out.println("The ticket price: "+fmt1.format(ticket_price));
+						System.out.println("Your Date of Booking: "+s1.getDate());
+						System.out.println("Days for stay: "+days);
+						System.out.println("Airport Destination: MSP");
+						
+					}
+					else if(Ans == 'N') {
+						System.out.print("Your ticket refund: "+fmt1.format(ticket_price));
+					}
+				}
+				
+				else
+				{
+					System.out.println("Exceeded the amount of days for a "
+							+ "road trip; which is 62. Please enter again");
+				}
+				
+				break;
+			}
+		
+			default:
+				System.out.println("Invalid input! Please enter one of the four options.");
+			break;
+		}
+		
+		
+	}
+
+}
---
+++ SDF_Route.java
@@ -0,0 +1,109 @@
+package Project2;
+import java.util.Scanner;
+
+
+public class SDF_Route {
+
+	private String Date;
+	private String FlightTicket;
+
+	//Displays routes from Louisville within the States
+	public void display_route()
+	{
+		System.out.println("This is Delta Airlines. "
+				+ "\nThis shows the avaliable routes from "
+				+ "\nLouisville with Delta Airlines.");
+		System.out.println("Airline IATA code: DL");
+		
+		System.out.println("ATL: Alanta, Georgia.");
+		System.out.println("ATL Flight Duration: 1 hour & 28 minutes.");
+		System.out.println("");
+		System.out.println("LGA: New York.");
+		System.out.println("LGA Flight Duration: 2 hours & 13 minutes.");
+		System.out.println("");
+		System.out.println("DTW: Detroit, Michigin.");
+		System.out.println("DTW Flight Duration: 1 hour & 28 minutes.");
+		System.out.println("");
+		System.out.println("MSP: Minneapolis.");
+		System.out.println("MSP Flight Duration: 2 hours & 10 minutes.");
+		
+	}
+	
+	public static boolean hasBook(boolean b)
+	{
+		return false;
+		
+	}
+	
+	
+	public void Date()
+	{
+		Scanner scan = new Scanner(System.in);
+		int mnth;
+		int day;
+		int year;
+		
+		System.out.println("Enter your date for booking");
+		 mnth = scan.nextInt();
+		 day = scan.nextInt();
+		 year = scan.nextInt();
+		
+		
+		if(mnth == 1 && mnth <= 12 
+				&& day == 01 && day <= 31 
+				&& year == 2022 || day <= 2023)
+		{
+		this.Date = Date;
+		String Date = mnth +"/"+day+"/"+year;
+		
+			System.out.println(Date);	
+		}
+		
+		
+		else if(mnth == 1 && mnth <= 12 
+				&& day == 1 && day <= 31 
+				&& year == 2022 || day <= 2023
+				&& year % 4 == 0)
+		{
+			this.Date = Date;
+			String Date = mnth +"/"+day+"/"+year;
+			System.out.println(Date);
+			
+		}
+		else
+		{
+			System.out.println("Invalid input! Please"
+					+ " enter a valid input.");
+		}
+		
+	}
+	
+	public String generateFlightNum(int length)
+	{
+		String FlightTicketSet = "1234567890";
+		
+		length = 13;
+		
+		char [] FlightTicket = new char [length];//an array to hold the values of the Flight ticket values
+		
+		for(int i = 0; i < length; i++)
+		{
+			int rand1 = (int) Math.random() * FlightTicketSet.length();//Generates random Password
+			FlightTicket[i] = FlightTicketSet.charAt(rand1); //Gets values from randomly generated places
+		}
+		return new String (FlightTicket);//returns the value to a String
+		
+	}
+	
+	public void FlightNum1()
+	{
+		this.FlightTicket = generateFlightNum(13);
+		System.out.println("Your Flight Ticket: "+this.FlightTicket);
+	}
+	
+	public String getDate()
+	{
+		this.Date = Date;
+		return Date;
+	}
+}
